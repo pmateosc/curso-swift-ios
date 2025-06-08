9c7c83e2e2880576a91f33f4c65e40b980b058fc
Pablo Mateos <pablo.mateosdev@gmail.com>
2025-06-08 21:38:34 +0200

    Initial Commit

---
+++ CursoiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CursoiOS
+//
+//  Created by PABLO MATEOS, Vodafone on 8/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by PABLO MATEOS, Vodafone on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
