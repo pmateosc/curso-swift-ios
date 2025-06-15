a23f1038804617377203a5f540c48f5261e9a52d
Pablo Mateos <pablo.mateosc@gmail.com>
2025-06-15 22:21:54 +0200

    FavPlaces App: - initial map configuration - Custom dialog component to save places on tap

---
+++ CursoiOS/FavPlaces/FavPlaces.swift
@@ -0,0 +1,18 @@
+//
+//  FavPlaces.swift
+//  CursoiOS
+//
+//  Created by PABLO MATEOS, Vodafone on 15/6/25.
+//
+
+import SwiftUI
+
+struct FavPlaces: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavPlaces()
+}
---
+++ CursoiOS/FavPlaces/model/Place.swift
@@ -0,0 +1,8 @@
+//
+//  Place.swift
+//  CursoiOS
+//
+//  Created by PABLO MATEOS, Vodafone on 15/6/25.
+//
+
+import Foundation
